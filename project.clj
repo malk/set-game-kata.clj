177e17340c12600d993d668f30e479ba85e109d5
malk <malk@zameth.org>
2016-08-10 23:51:21 +0200

    tried a first solution

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha10"] [org.clojure/test.check "0.9.0"]]
-  :plugins [[cider/cider-nrepl "0.8.1"] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.3"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"] [org.clojure/test.check "0.9.0"]
+                 [org.clojure/math.combinatorics "0.1.3"]]
+  :plugins [[cider/cider-nrepl "0.8.1"] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.3"]
+            [lein-bikeshed "0.3.0"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/set_game_kata/clj.clj
+++ src/set_game_kata/clj.clj
@@ -1,7 +1,44 @@
 (ns set-game-kata.clj
-  (:gen-class))
+  (:require [clojure.math.combinatorics :refer [combinations]]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def quantity? #{1 2 3})
+(def shading? #{:solid :stripped :outlined})
+(def color? #{:red :purple :green})
+(def shape? #{:oval :squiggle :diamond})
+
+(def card?
+  (set
+   (for [quantity quantity?
+         shading shading?
+         color color?
+         shape shape?]
+     [quantity shading color shape])))
+
+(defn set-of-features?
+  "true when all given features are identical or when they all differ"
+  [features]
+  (or
+   (apply = features)
+   (apply distinct? features)))
+
+(defn set-of-cards?
+  "true when all given cards have only features that pass set-of-features?"
+  [cards]
+  (every? set-of-features? (apply (partial map list) cards)))
+
+(defn set-game?
+  "true when any 3 of the given cards form a set-of-cards"
+  [cards]
+  (some set-of-cards? (combinations cards 3)))
+
+(defn game-hand?
+  "true when given hand is composed of 12 cards"
+  [hand]
+  (and
+   (= 12 (count hand))
+   (every? card? hand)))
+
+(defn deal
+  "generates a game hand"
+  []
+  (take 12 (shuffle card?)))
