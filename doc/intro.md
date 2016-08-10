cf8f4b3a79e12ed313926aa5bae75367618d849d
malk <malk@zameth.org>
2016-08-10 03:06:19 +0200

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to set-game-kata.clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/set_game_kata/clj_test.clj
@@ -0,0 +1,7 @@
+(ns set-game-kata.clj-test
+  (:require [clojure.test :refer :all]
+            [set-game-kata.clj :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
