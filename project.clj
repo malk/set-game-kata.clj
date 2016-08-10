beb8c1fa6167406dc3a1a153ba1428a18537bb90
malk <malk@zameth.org>
2016-08-10 03:10:05 +0200

    added usual deps

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :main ^:skip-aot set-game-kata.clj
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"] [org.clojure/test.check "0.9.0"]]
+  :plugins [[cider/cider-nrepl "0.8.1"] [lein-kibit "0.1.2"] [jonase/eastwood "0.2.3"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
